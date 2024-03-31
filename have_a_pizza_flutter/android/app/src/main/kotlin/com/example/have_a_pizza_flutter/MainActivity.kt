a3578e16ddfee75946cfbf75b7a3537b7351e46d
mohamed abderraouf douadi <me.is.intik@gmail.com>
2024-03-31 13:28:02 +0100

    initial commit

---
+++ have_a_pizza_flutter/android/app/src/main/kotlin/com/example/have_a_pizza_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.have_a_pizza_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ have_a_pizza_server/bin/main.dart
@@ -0,0 +1,7 @@
+import 'package:have_a_pizza_server/server.dart';
+
+/// This is the starting point for your Serverpod server. Typically, there is
+/// no need to modify this file.
+void main(List<String> args) {
+  run(args);
+}
